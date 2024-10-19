4eaa37b840c3060cb835cbd59e326ee1e955ea6c
jmathies <jakob.mathieson@uvm.edu>
2024-10-19 19:33:26 -0400

    Added GroupsMainView

---
+++ giftApp/PeopleViews/GroupsMainView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupsMainView.swift
+//  giftApp
+//
+//  Created by jmathies on 10/16/24.
+//
+
+import SwiftUI
+
+struct GroupsMainView: View {
+    var body: some View {
+        Text("Groups View")
+    }
+}
+
+#Preview {
+    GroupsMainView()
+}
